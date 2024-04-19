3b8cd39f675398d41676893ccb0cd7987d54f759
atiks88 <141978852+atiks88@users.noreply.github.com>
2024-04-19 03:10:18 +0000

    my final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+
+  const [showResult, setShowResult] = useState(false);
+  const [counter, setCounter] = useState('');
+
+  const Greeting = (e) => {
+    setCounter(e.target.value);
+    setShowResult(true); 
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+       <input type="text" onChange={Greeting}></input>
+       {showResult && <p>Hello {counter}</p>}
     </div>
   )
 }
